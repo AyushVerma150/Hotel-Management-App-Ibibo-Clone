32bf27b84ce3a9da7db842bb46a57b1c26954ef2
AyushVerma150 <64376745+AyushVerma150@users.noreply.github.com>
2021-05-10 20:56:05 +0530

    Some More Reafctors

--- hotel-management-app/src/Components/Checkout/Checkout.js
+++ hotel-management-app/src/Components/Checkout/Checkout.js
@@ -5,25 +5,23 @@ import { createBooking } from "Components/Hotel/HotelSlice";
 import moment from "moment";
 import { Button } from "react-bootstrap";
 import CardComponent from "UI/CardComponent";
+import styles from "Components/Checkout/Checkout.module.css";
 
 const Checkout = () => {
   const dispatch = useDispatch();
-
+  const bookingStatus = useSelector((state) => state.hotel.bookingStatus);
   const selectedHotels = useSelector((state) => state.hotel.hotelSelected);
   const currentUser = useSelector((state) => state.user.currentUser);
-  console.log(currentUser);
   const [content, setContent] = useState(null);
+
   const startDate = moment(selectedHotels[0].checkIn, "YYYY-MM-DD");
   const endDate = moment(selectedHotels[0].checkOut, "YYYY-MM-DD");
   const days = endDate.diff(startDate, "days");
-  console.log(days);
-  // console.log( array.map( x => parseInt( x ) ) );
   const roomCharges = selectedHotels[0].discountPrice;
   const rooms = selectedHotels[0].rooms;
   const nightsSpent = days;
   const discount = selectedHotels[0].price - selectedHotels[0].discountPrice;
   const tax = 498;
-  const bookingStatus = useSelector((state) => state.hotel.bookingStatus);
 
   const handleCreateBooking = () => {
     const bookingsData = {
@@ -38,37 +36,19 @@ const Checkout = () => {
       hotelId: selectedHotels[0].id,
       userId: currentUser.id,
     };
-
-    console.log(bookingsData);
     dispatch(createBooking(bookingsData));
   };
 
   useEffect(() => {
     if (bookingStatus === "success") {
       setContent(
-        <div
-          style={{
-            height: "40px",
-            width: "50%",
-            fontFamily: "Poppins",
-            color: "green",
-            marginLeft: "25%",
-          }}
-        >
+        <div className={styles.successDiv}>
           <strong>Booking Made Successfully</strong>
         </div>
       );
     } else if (bookingStatus === "failed") {
       setContent(
-        <div
-          style={{
-            height: "40px",
-            width: "50%",
-            fontFamily: "Poppins",
-            color: "red",
-            marginLeft: "25%",
-          }}
-        >
+        <div className={styles.errorDiv}>
           <strong>Booking Failed</strong>
         </div>
       );
@@ -77,31 +57,9 @@ const Checkout = () => {
 
   return (
     <div>
-      {/* //outer most div */}
-      <div style={{ backgroundColor: "dodgerblue", height: "300px" }}>
-        {content}
-      </div>
-      <div
-        style={{
-          display: "flex",
-          backgroundColor: "transparent",
-          marginTop: "-279px",
-        }}
-      >
-        <div
-          style={{
-            color: "black",
-            display: "block",
-            backgroundColor: "white",
-            borderRadius: "5px",
-            left: "0",
-            width: "60%",
-            marginLeft: "30px",
-            marginRight: "30px",
-            boxShadow:
-              " 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-          }}
-        >
+      <div className={styles.checkoutOuterDiv}>{content}</div>
+      <div className={styles.checkoutInnerDiv}>
+        <div className={styles.checkoutCardDiv}>
           {selectedHotels.map((hotel) => {
             return (
               <CardComponent
@@ -128,39 +86,21 @@ const Checkout = () => {
                     para: "",
                     type: "button",
                     content: (
-                      <div
-                        style={{
-                          display: "flex",
-                          fontSize: "1rem",
-                          color: "black",
-                        }}
-                      >
-                        <div style={{ left: "0", width: "20%" }}>
-                          <strong style={{ fontFamily: "Poppins" }}>
-                            {" "}
-                            Check In
-                          </strong>
-                          <p style={{ fontFamily: "Poppins" }}>
-                            {hotel.checkIn}
-                          </p>
+                      <div className={styles.checkoutDivContent}>
+                        <div className={styles.leftDiv}>
+                          <strong>Check In</strong>
+                          <p>{hotel.checkIn}</p>
                         </div>
-                        <div style={{ right: "0", width: "20%" }}>
-                          <strong style={{ fontFamily: "Poppins" }}>
-                            Check Out
-                          </strong>
-                          <p style={{ fontFamily: "Poppins" }}>
-                            {hotel.checkOut}
-                          </p>
+                        <div className={styles.rightDiv}>
+                          <strong>Check Out</strong>
+                          <p>{hotel.checkOut}</p>
                         </div>
-                        <div style={{ right: "0", width: "20%" }}>
-                          <strong style={{ fontFamily: "Poppins" }}>
-                            Guests
-                          </strong>
-                          <p style={{ fontFamily: "Poppins" }}>
+                        <div className={styles.rightDiv}>
+                          <strong>Guests</strong>
+                          <p>
                             {hotel.adults +
                               hotel.children +
-                              " Guests " +
-                              " | " +
+                              " Guests  | " +
                               hotel.rooms +
                               " Room"}
                           </p>
@@ -177,231 +117,86 @@ const Checkout = () => {
             );
           })}
         </div>
-        {/* //   <Card */}
-        {/* //     style={{
-            //       border: "none",
-            //       color: "black",
-            //       boxShadow:
-            //         " 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-            //     }}
-            //   >
-            //     <label
-            //       style={{
-            //         fontFamily: "Poppins",
-            //         fontSize: "1.2rem",
-            //         padding: "5px",
-            //       }}
-            //     >
-            //       <strong>Hotel Info</strong>
-            //     </label>
-            //     <hr style={{ backgroundColor: "gray" }} />
-            //     <Card.Body>
-            //       <Card.Img
-            //         style={{ height: "200px", width: "50%" }}
-            //         src="https://cdn1.goibibo.com/voy_ing/t_g/81ad700a81ee11e486f9daf4768ad8d9.jfif"
-            //         alt="not available"
-            //       />
-            //       <Card.Title>
-            //         <div
-            //           style={{
-            //             display: "block",
-            //             fontSize: "1rem",
-            //             color: "black",
-            //           }}
-            //         >
-            //           <strong style={{ fontFamily: "Poppins" }}>Name</strong>
-            //           <p
-            //             style={{
-            //               fontFamily: "Poppins",
-            //               color: "black",
-            //             }}
-            //           >
-            //             {hotel.name}
-            //           </p>
-            //         </div>
-            //       </Card.Title>
-            //       <Card.Title>
-            //         <div
-            //           style={{
-            //             display: "block",
-            //             fontSize: "1rem",
-            //             color: "black",
-            //           }}
-            //         >
-            //           <strong style={{ fontFamily: "Poppins" }}>
-            //             <i class="fas fa-map-marked-alt"></i> Locate On Map
-            //           </strong>
-            //           <p style={{ fontFamily: "Poppins" }}>{hotel.location}</p>
-            //         </div>
-            //       </Card.Title>
-            //       <Card.Title>
-            //         <div
-            //           style={{
-            //             display: "flex",
-            //             fontSize: "1rem",
-            //             color: "black",
-            //           }}
-            //         >
-            //           <div style={{ left: "0", width: "20%" }}>
-            //             <strong style={{ fontFamily: "Poppins" }}>
-            //               {" "}
-            //               Check In
-            //             </strong>
-            //             <p style={{ fontFamily: "Poppins" }}>{hotel.checkIn}</p>
-            //           </div>
-            //           <div style={{ right: "0", width: "20%" }}>
-            //             <strong style={{ fontFamily: "Poppins" }}>
-            //               Check Out
-            //             </strong>
-            //             <p style={{ fontFamily: "Poppins" }}>
-            //               {hotel.checkOut}
-            //             </p>
-            //           </div>
-            //           <div style={{ right: "0", width: "20%" }}>
-            //             <strong style={{ fontFamily: "Poppins" }}>
-            //               Guests
-            //             </strong>
-            //             <p style={{ fontFamily: "Poppins" }}>
-            //               {hotel.adults +
-            //                 hotel.children +
-            //                 " Guests " +
-            //                 " | " +
-            //                 hotel.rooms +
-            //                 " Room"}
-            //             </p>
-            //           </div>
-            //         </div>
-            //       </Card.Title>
-            //       <Card.Title>
-            //         <div
-            //           style={{
-            //             display: "block",
-            //             fontSize: "1rem",
-            //             color: "black",
-            //           }}
-            //         >
-            //           <strong style={{ fontFamily: "Poppins" }}>
-            //             {" "}
-            //             Room Selected
-            //           </strong>
-            //           <p style={{ fontFamily: "Poppins" }}>
-            //             {hotel.type + " " + "x" + hotel.rooms}
-            //           </p>
-            //         </div>
-            //       </Card.Title>
-            //     </Card.Body>
-            //   </Card>
-            // );
-         */}
-
-        <div
-          style={{
-            height: "300px",
-            backgroundColor: "white",
-            borderRadius: "5px",
-            right: "0",
-            width: "30%",
-            marginLeft: "10px",
-            marginRight: "30px",
-            boxShadow:
-              " 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-          }}
-        >
-          <Card style={{ border: "none", color: "black" }}>
-            <label
-              style={{
-                fontFamily: "Poppins",
-                fontSize: "1.2rem",
-                padding: "5px",
-              }}
-            >
-              <strong>Price Summary</strong>
-            </label>
-            <hr style={{ backgroundColor: "gray" }} />
-            <Card.Body>
-              <Card.Title>
-                <div
-                  style={{ display: "flex", fontSize: "1rem", color: "black" }}
-                >
-                  <div style={{ left: "0", width: "80%" }}>
-                    <strong style={{ fontFamily: "Poppins" }}>
-                      Room Charges (
-                      {rooms + " rooms x " + nightsSpent + " nights"})
-                    </strong>
+        <div className={styles.priceSummaryDiv}>
+          <CardComponent
+            style={{
+              fontFamily: "Poppins",
+              fontSize: "1.2rem",
+              padding: "5px",
+            }}
+            cardTitle={[
+              {
+                type: "button",
+                content: (
+                  <div className={styles.contentOuterDiv}>
+                    <div className={styles.contentInnerLeftDiv}>
+                      <strong>
+                        Room Charges (
+                        {rooms + " rooms x " + nightsSpent + " nights"})
+                      </strong>
+                    </div>
+                    <div className={styles.contentInnerRightDiv}>
+                      <strong>{"Rs. " + roomCharges * nightsSpent}</strong>
+                    </div>
                   </div>
-                  <div style={{ right: "0", width: "20%" }}>
-                    <strong style={{ fontFamily: "Poppins" }}>
-                      {"Rs. " + roomCharges * nightsSpent}
-                    </strong>
+                ),
+              },
+              {
+                type: "button",
+                content: (
+                  <div className={styles.contentOuterDiv}>
+                    <div className={styles.contentInnerLeftDiv}>
+                      <strong>Discount</strong>
+                    </div>
+                    <div className={styles.contentInnerRightDiv}>
+                      <strong>{"Rs. " + discount}</strong>
+                    </div>
                   </div>
-                </div>
-              </Card.Title>
-              <Card.Title>
-                <div
-                  style={{ display: "flex", fontSize: "1rem", color: "black" }}
-                >
-                  <div style={{ left: "0", width: "80%" }}>
-                    <strong style={{ fontFamily: "Poppins" }}>Discount</strong>
+                ),
+              },
+              {
+                type: "button",
+                content: (
+                  <div className={styles.contentOuterDiv}>
+                    <div className={styles.contentInnerLeftDiv}>
+                      <strong>Taxes and Fees</strong>
+                    </div>
+                    <div className={styles.contentInnerRightDiv}>
+                      <strong>Rs. 498</strong>
+                    </div>
                   </div>
-                  <div style={{ right: "0", width: "20%" }}>
-                    <strong style={{ fontFamily: "Poppins" }}>
-                      {"Rs. " + discount}
-                    </strong>
+                ),
+              },
+              {
+                type: "button",
+                content: (
+                  <div className={styles.contentOuterDiv}>
+                    <div className={styles.contentInnerLeftDiv}>
+                      <strong>Pay Now</strong>
+                    </div>
+                    <div className={styles.contentInnerRightDiv}>
+                      <strong>
+                        {"Rs. " + (roomCharges * nightsSpent - discount + tax)}
+                      </strong>
+                    </div>
                   </div>
-                </div>
-              </Card.Title>
-              <Card.Title>
-                <div
-                  style={{ display: "flex", fontSize: "1rem", color: "black" }}
-                >
-                  <div style={{ left: "0", width: "80%" }}>
-                    <strong style={{ fontFamily: "Poppins" }}>
-                      Taxes and Fees
-                    </strong>
-                  </div>
-                  <div style={{ right: "0", width: "20%" }}>
-                    <strong style={{ fontFamily: "Poppins" }}>
-                      {"Rs. " + "498"}
-                    </strong>
-                  </div>
-                </div>
-              </Card.Title>
-              <hr />
-              <Card.Title>
-                <div
-                  style={{ display: "flex", fontSize: "1rem", color: "black" }}
-                >
-                  <div style={{ left: "0", width: "80%" }}>
-                    <strong style={{ fontFamily: "Poppins" }}>Pay Now</strong>
-                  </div>
-                  <div style={{ right: "0", width: "20%" }}>
-                    <strong style={{ fontFamily: "Poppins" }}>
-                      {"Rs. " + (roomCharges * nightsSpent - discount + tax)}
-                    </strong>
-                  </div>
-                </div>
-              </Card.Title>
-              <Card.Title>
-                <Button
-                  onClick={() => {
-                    handleCreateBooking();
-                  }}
-                  style={{
-                    marginLeft: "25%",
-                    border: "none",
-                    outline: "none",
-                    color: "white",
-                    backgroundColor: "rgb(255, 109, 56)",
-                    height: "50px",
-                    width: "200px",
-                    borderRadius: "10px",
-                  }}
-                >
-                  Create Booking
-                </Button>
-              </Card.Title>
-            </Card.Body>
-          </Card>
+                ),
+              },
+              {
+                type: "button",
+                content: (
+                  <Button
+                    onClick={() => {
+                      handleCreateBooking();
+                    }}
+                    className={styles.createBookingButton}
+                  >
+                    Create Booking
+                  </Button>
+                ),
+              },
+            ]}
+          />
         </div>
       </div>
     </div>
